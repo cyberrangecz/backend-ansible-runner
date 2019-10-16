add7a2fa7f4c12140f9845986870aaccd83a63cc
172673 <172673@muni.cz>
2019-10-16 14:48:07 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM alpine:3.10
+
+ENV ANSIBLE_STDOUT_CALLBACK=default
+ENV ANSIBLE_RETRY_FILES_ENABLED=0
+
+RUN apk --update add --no-cache ansible bash git openssh
+
+COPY ./kypo-ansible-runner.sh /app/
+
+WORKDIR /app
+
+# /app/inventory.ini
+ENTRYPOINT ["./kypo-ansible-runner.sh", "-i", "inventory.ini"]
+# docker inspect c2c769c4b9ef --format='{{.State.ExitCode}}'
---
+++ kypo-ansible-runner.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+usage() { echo "kypo-ansible-runner.sh -r [git repo url] -i [inventory file path]"; }
+
+while getopts ":r:i:h" opt; do
+  case ${opt} in
+  r) REPO_URL=$OPTARG ;;
+  i) INVENTORY=$OPTARG ;; # realpath -e on some systems
+  h)
+    usage
+    exit
+    ;;
+  \?)
+    echo "Unknown option: -$OPTARG" >&2
+    exit 1
+    ;;
+  :)
+    echo "Missing option argument for -$OPTARG" >&2
+    exit 1
+    ;;
+  *)
+    echo "Unimplemented option: -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+done
+
+if ((OPTIND == 1)); then
+  echo "No options specified"
+  exit 1
+fi
+shift $((OPTIND - 1))
+
+chmod 600 /root/.ssh -R
+
+# Check if inventory file exists.
+INVENTORY_FILE=$(realpath $INVENTORY)
+if [ $? != 0 ]; then
+  echo "Inventory file does not exist. Exiting."
+  exit 1
+fi
+
+# Check if git repo is reachable.
+git ls-remote $REPO_URL -q
+if [ $? != 0 ]; then
+  echo "Git repository unreachable. Git exit code: ${REPO_TEST}. Exiting."
+  exit 1
+fi
+
+git clone $REPO_URL ansible_repo
+cd ansible_repo/provisioning || exit 1
+
+REQUIREMENTS_FILE="requirements.yml"
+if [ -f $REQUIREMENTS_FILE ]; then
+  ansible-galaxy install -r $REQUIREMENTS_FILE
+fi
+
+PLAYBOOK_FILE="playbook.yml"
+ansible-playbook $PLAYBOOK_FILE -i "${INVENTORY_FILE}"
